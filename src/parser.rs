92e816c99b1000e8115299aba9ddf8c2e1c591bf
Taiki Harada <haradataiki19990927@gmail.com>
2021-04-29 16:10:46 +0900

    パースの仕様変更

--- src/parser.rs
+++ src/parser.rs
@@ -7,31 +7,27 @@ mod tests {
     use crate::ast::{Expr, Op};
     use crate::parser::expr;
 
-    fn parse(input: &str) -> Box<Expr> {
+    fn parse(input: &str) -> Expr {
         expr::ExprParser::new().parse(input).unwrap()
     }
 
     #[test]
     fn parse_num() {
-        assert_eq!(Box::new(Expr::Num(1)), parse("1"));
-        assert_eq!(Box::new(Expr::Num(1)), parse("(1)"));
-        assert_eq!(Box::new(Expr::Num(1)), parse("((1))"));
+        assert_eq!(Expr::Num(1), parse("1"));
+        assert_eq!(Expr::Num(1), parse("(1)"));
+        assert_eq!(Expr::Num(1), parse("((1))"));
     }
 
     #[test]
     fn parse_bool() {
-        assert_eq!(Box::new(Expr::Bool(true)), parse("true"));
-        assert_eq!(Box::new(Expr::Bool(false)), parse("false"));
+        assert_eq!(Expr::Bool(true), parse("true"));
+        assert_eq!(Expr::Bool(false), parse("false"));
     }
 
     #[test]
     fn parse_add() {
         assert_eq!(
-            Box::new(Expr::BinOp(
-                Box::new(Expr::Num(1)),
-                Op::Add,
-                Box::new(Expr::Num(2)),
-            )),
+            Expr::BinOp(Box::new(Expr::Num(1)), Op::Add, Box::new(Expr::Num(2)),),
             parse("1+2")
         );
     }
@@ -39,11 +35,7 @@ mod tests {
     #[test]
     fn parse_mul() {
         assert_eq!(
-            Box::new(Expr::BinOp(
-                Box::new(Expr::Num(1)),
-                Op::Mul,
-                Box::new(Expr::Num(2)),
-            )),
+            Expr::BinOp(Box::new(Expr::Num(1)), Op::Mul, Box::new(Expr::Num(2)),),
             parse("1*2")
         );
     }
